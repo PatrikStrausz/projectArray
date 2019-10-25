57b915974e49e59aedc408f9cb760015abd31c40
patrikstrausz <patrik.strausz@akademiasovy.sk>
2019-10-25 10:38:54 +0200

    1234

---
+++ src/sk/itsovy/strausz/school/Main.java
@@ -0,0 +1,10 @@
+package sk.itsovy.strausz.school;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Student damian = new Student("damian",20,"1N");
+
+    }
+}
---
+++ src/sk/itsovy/strausz/school/Student.java
@@ -0,0 +1,19 @@
+package sk.itsovy.strausz.school;
+
+public class Student extends Person {
+    private String group;
+
+    public Student(String name, int age, String group) {
+        super(name, age);
+        this.group = group;
+        System.out.println("Im student");
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+}
