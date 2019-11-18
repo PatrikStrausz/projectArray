eba9b3a2e16892675f4ae14aeacec47a774d208b
PatrikStrausz <55127576+PatrikStrausz@users.noreply.github.com>
2019-11-18 18:53:46 +0100

    Update Main.java

--- src/sk/itsovy/strausz/array2D/Main.java
+++ src/sk/itsovy/strausz/array2D/Main.java
@@ -3,110 +3,80 @@ package sk.itsovy.strausz.array2D;
 import java.util.Random;
 
 public class Main {
-    private final static int numRows = 4;
+   private final static int numRows = 4;
     private final static int numCols = 4;
 
     public static void main(String[] args) {
-
-
         Random rnd = new Random();
-        int[][] array;
+        int arr[][] = new int[numRows][numCols];
         int r, c;
-
-
-        array = new int[numRows][numCols];
-
-        int maxColSum = 0;
         for (r = 0; r < numRows; r++) {
             for (c = 0; c < numCols; c++) {
-                array[r][c] = rnd.nextInt(10) ;
-
+                arr[r][c] = rnd.nextInt(10);
             }
-
         }
-        int min = array[0][0];
-        int max = array[0][0];
 
-        int column = 0;
-        int sum = 0;
 
+        int max = arr[0][0];
         for (r = 0; r < numRows; r++) {
             for (c = 0; c < numCols; c++) {
-                if (array[r][c] < 10)
+                if (arr[r][c] < 10) {
                     System.out.print(" ");
-
-
-                if (max < array[r][c]) {
-                    max = array[r][c];
-                }
-
-                if (min > array[r][c]) {
-                    min = array[r][c];
                 }
 
 
-                System.out.print(array[r][c] + "  ");
+                System.out.print(arr[r][c] + " ");
+            }
+            System.out.println();
+        }
 
+        for (r = 0; r < numRows; r++) {
+            for (c = 0; c < numCols; c++) {
 
+                if (max < arr[r][c]) {
+                    max = arr[r][c];
+                }
+            }
+        }
+        System.out.println("Max is: " +max);
+
+        int maxColSum=0;
+
+        for(r=0;r<numRows;r++)
+            maxColSum+=arr[r][0];
+
+        int sum=0;
+        for(c=1;c<numCols;c++) { // spocita sucet riadkov a vypise najvacsi
+            sum = 0;
+            for (r = 0; r < numRows; r++)
+                sum+=arr[r][c];
+            if(sum>maxColSum){
+                maxColSum=sum;
 
             }
-
-
-            System.out.println();
-
-
         }
+        System.out.println("Max column value : "+ " [ sum: "+ maxColSum+" ]");
 
-        System.out.println();
-        System.out.println("Max is: " + max);
-        System.out.println("Min is: " + min);
-
-
-//        int maxColSum = 0;
-//        int column = 0;
-//        int sum = 0;
-//
-//        for (r = 0; r < numRows; r++) {
-//            maxColSum += array[r][0];
-//        }
-//
-//        for (c = 1; c < numCols; c++)
-//            sum = 0;
-//        for (r = 0; r < numRows; r++)
-//            sum += array[r][c];   // spocita sucet riadkov a vypise najvacsi
-//        if (sum > maxColSum) {
-//            maxColSum = sum;
-//            column = c;
-//
-//        }
-
-
-
-//        System.out.println("Max column value: " + column + " sum: " + maxColSum);
-
-        int minRc = numCols<numRows?numCols:numRows;
-        int mult=1;
-        for(r =0;r<minRc;r++){
-            mult*=array[r] [r];
+        int minRC=numCols<numRows?numCols:numRows;
+        int mul=1;
+        for(r=0;r<minRC;r++) { // vynasobi cisla v diagonale
+            mul*=arr[r][r];
         }
-        System.out.println("Mul of main diagonal is: " +mult);
-
+        System.out.println("Multiplication of main diagonale is: "+mul);
 
         for (c = 0; c < numCols; c++) {
+           for (r = 0; r < numRows; r++) {
+               if(arr[r][c] < 10 ){
+                   System.out.print(" ");
+               }
+               System.out.print(arr[r] [c] +" "); // transparentne vypise pole
+             }
+            System.out.println();
+           }
+        System.out.println();
 
-            for (r = 0; r < numRows; r++) {
+        int[][] tictactoe = {{1,0,2},{0,1,2}, {0,2,0}};
 
-                if (array[r][c] < 10)
-                    System.out.print(" ");
-                System.out.print(array[r] [c] + " "); // transparentne vypise pole
-            }
-            System.out.println( );
-        }
-
-
-
- int [][]ticTacToe = {{1,0,2}, {0,1,2}, {0,2,0}};
-        ticTacToe= new int[3][3];
 
     }
 
