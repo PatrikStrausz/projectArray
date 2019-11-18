37bec1430a624f6c1571b0c8cc02480b7f74ed14
patrikstrausz <patrik.strausz@akademiasovy.sk>
2019-11-18 12:33:16 +0100

    1234

--- src/sk/itsovy/strausz/array2D/Main.java
+++ src/sk/itsovy/strausz/array2D/Main.java
@@ -16,16 +16,19 @@ public class Main {
 
         array = new int[numRows][numCols];
 
-
+        int maxColSum = 0;
         for (r = 0; r < numRows; r++) {
             for (c = 0; c < numCols; c++) {
-                array[r][c] = rnd.nextInt(100) + 1;
+                array[r][c] = rnd.nextInt(10) ;
+
             }
 
         }
         int min = array[0][0];
         int max = array[0][0];
 
+        int column = 0;
+        int sum = 0;
 
         for (r = 0; r < numRows; r++) {
             for (c = 0; c < numCols; c++) {
@@ -43,6 +46,9 @@ public class Main {
 
 
                 System.out.print(array[r][c] + "  ");
+
+
+
             }
 
 
@@ -51,30 +57,57 @@ public class Main {
 
         }
 
-
         System.out.println();
         System.out.println("Max is: " + max);
         System.out.println("Min is: " + min);
 
-        int maxColSum = 0;
-        int column = 0;
-        int sum = 0;
 
-        for (r = 0; r < numRows; r++) {
-            maxColSum += array[r][0];
+//        int maxColSum = 0;
+//        int column = 0;
+//        int sum = 0;
+//
+//        for (r = 0; r < numRows; r++) {
+//            maxColSum += array[r][0];
+//        }
+//
+//        for (c = 1; c < numCols; c++)
+//            sum = 0;
+//        for (r = 0; r < numRows; r++)
+//            sum += array[r][c];   // spocita sucet riadkov a vypise najvacsi
+//        if (sum > maxColSum) {
+//            maxColSum = sum;
+//            column = c;
+//
+//        }
+
+
+
+//        System.out.println("Max column value: " + column + " sum: " + maxColSum);
+
+        int minRc = numCols<numRows?numCols:numRows;
+        int mult=1;
+        for(r =0;r<minRc;r++){
+            mult*=array[r] [r];
+        }
+        System.out.println("Mul of main diagonal is: " +mult);
+
+
+        for (c = 0; c < numCols; c++) {
+
+            for (r = 0; r < numRows; r++) {
+
+                if (array[r][c] < 10)
+                    System.out.print(" ");
+                System.out.print(array[r] [c] + " "); // transparentne vypise pole
+            }
+            System.out.println( );
         }
 
-        for (c = 1; c < numCols; c++)
-            sum = 0;
-        for (r = 0; r < numRows; r++)
-            sum += array[r][c];   // spocita sucet riadkov a vypise najvacsi
-        if (sum > maxColSum) {
-            maxColSum = sum;
-            column = c;
-
-        }
 
 
-        System.out.println("Max column value: " + column + " sum: " + maxColSum);
+ int [][]ticTacToe = {{1,0,2}, {0,1,2}, {0,2,0}};
+        ticTacToe= new int[3][3];
+
     }
+
 }
