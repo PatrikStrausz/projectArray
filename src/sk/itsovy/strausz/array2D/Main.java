492c221d9f824317e87a49e0a55bfd744521c2da
patrikstrausz <patrik.strausz@akademiasovy.sk>
2019-11-19 12:07:53 +0100

    1234

--- src/sk/itsovy/strausz/array2D/Main.java
+++ src/sk/itsovy/strausz/array2D/Main.java
@@ -9,105 +9,206 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Random rnd = new Random();
-        int[][] array;
-        int r, c;
-
-
-        array = new int[numRows][numCols];
-
-        int maxColSum = 0;
-        for (r = 0; r < numRows; r++) {
-            for (c = 0; c < numCols; c++) {
-                array[r][c] = rnd.nextInt(10) ;
-
-            }
-
-        }
-        int min = array[0][0];
-        int max = array[0][0];
-
-        int column = 0;
-        int sum = 0;
-
-        for (r = 0; r < numRows; r++) {
-            for (c = 0; c < numCols; c++) {
-                if (array[r][c] < 10)
-                    System.out.print(" ");
-
-
-                if (max < array[r][c]) {
-                    max = array[r][c];
-                }
-
-                if (min > array[r][c]) {
-                    min = array[r][c];
-                }
-
-
-                System.out.print(array[r][c] + "  ");
-
-
-
-            }
-
-
-            System.out.println();
-
-
-        }
-
-        System.out.println();
-        System.out.println("Max is: " + max);
-        System.out.println("Min is: " + min);
-
-
-//        int maxColSum = 0;
-//        int column = 0;
-//        int sum = 0;
+//        Random rnd = new Random();
+//        int arr[][] = new int[numRows][numCols];
+//        int r, c;
+//        for (r = 0; r < numRows; r++) {
+//            for (c = 0; c < numCols; c++) {
+//                arr[r][c] = rnd.nextInt(10);
+//            }
+//        }
+//
+//
+//        int max = arr[0][0];
+//        int countOdd = 0;
+//        int countEven =0;
+//        for (r = 0; r < numRows; r++) {
+//            for (c = 0; c < numCols; c++) {
+//                if (arr[r][c] < 10) {
+//                    System.out.print(" ");
+//                }
+//
+//
+//                System.out.print(arr[r][c] + " ");
+//            }
+//            System.out.println();
+//        }
 //
 //        for (r = 0; r < numRows; r++) {
-//            maxColSum += array[r][0];
-//        }
+//            for (c = 0; c < numCols; c++) {
 //
-//        for (c = 1; c < numCols; c++)
+//                if (max < arr[r][c]) {
+//                    max = arr[r][c];
+//                }
+//                if(arr[r] [c]%2==0){    // spocita parne cisla v poli
+//                    countEven++;
+//                }
+//                if(arr[r] [c]%2!=0){ // spocita neparne cisla v poli
+//                    countOdd++;
+//                }
+//
+//
+//            }
+//        }
+//        System.out.println("Max is: " +max);
+//        System.out.println("Count of odd numbers: " +countOdd);
+//        System.out.println("Count of even numbers: " +countEven);
+//
+//        int maxColSum=0;
+//
+//        for(r=0;r<numRows;r++)
+//            maxColSum+=arr[r][0];
+//
+//        int sum=0;
+//        for(c=1;c<numCols;c++) { // spocita sucet riadkov a vypise najvacsi
 //            sum = 0;
-//        for (r = 0; r < numRows; r++)
-//            sum += array[r][c];   // spocita sucet riadkov a vypise najvacsi
-//        if (sum > maxColSum) {
-//            maxColSum = sum;
-//            column = c;
+//            for (r = 0; r < numRows; r++)
+//                sum+=arr[r][c];
+//            if(sum>maxColSum){
+//                maxColSum=sum;
 //
+//            }
 //        }
+//        System.out.println("Max column value : "+ " [ sum: "+ maxColSum+" ]");
+//
+//        int minRC=numCols<numRows?numCols:numRows;
+//        int mul=1;
+//        for(r=0;r<minRC;r++) { // vynasobi cisla v diagonale
+//            mul*=arr[r][r];
+//        }
+//        System.out.println("Multiplication of main diagonale is: "+mul);
+//
+//        for (c = 0; c < numCols; c++) {
+//            for (r = 0; r < numRows; r++) {
+//                if(arr[r][c] < 10 ){
+//                    System.out.print(" ");
+//                }
+//                System.out.print(arr[r] [c] +" "); // transparentne vypise pole
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+//
+//
+//
+//
+//        for (r = 0; r < numRows; r++) {
+//            for (c = 0; c < numCols; c++) {
+//
+//                int index1 = rnd.nextInt(numRows);
+//                int index2 = rnd.nextInt(numCols);
+//                int index3 = rnd.nextInt(numRows);
+//                int index4 = rnd.nextInt(numCols); // zmeni nahodne cisla v poli
+//
+//
+//                int temp = arr[index1][index2];
+//                arr[index1][index2] = arr[index3][index4];
+//                arr[index3][index4] = temp;
+//
+//                System.out.print(arr[r][c] + " ");
+//            }
+//            System.out.println();
+//            }
+//        System.out.println();
+//
+//
+//
+//        int sum2=0;
+//        for(int i =0; i<numCols; i++){
+//            sum2 = sum2 + arr[0][i] + arr [numRows-1][i];
+//        }
+//        for(int i= 1; i<numRows-1;i++){
+//            sum2 = sum2 + arr[i][0] + arr[i] [numCols-1]; // spocita cisla po obvode
+//        }
+//
+//
+//
+//        System.out.println();
+//         System.out.println("Sum is: " +sum2);
+
+        int[][] ticTacToe = {{1, 0, 2}, {0, 1, 2}, {0, 2, 0}};
 
 
+        for (int i = 0; i < ticTacToe.length; i++) {
+            for (int j = 0; j < ticTacToe.length; j++) {
 
-//        System.out.println("Max column value: " + column + " sum: " + maxColSum);
-
-        int minRc = numCols<numRows?numCols:numRows;
-        int mult=1;
-        for(r =0;r<minRc;r++){
-            mult*=array[r] [r];
-        }
-        System.out.println("Mul of main diagonal is: " +mult);
-
-
-        for (c = 0; c < numCols; c++) {
-
-            for (r = 0; r < numRows; r++) {
-
-                if (array[r][c] < 10)
-                    System.out.print(" ");
-                System.out.print(array[r] [c] + " "); // transparentne vypise pole
+                System.out.print(ticTacToe[i][j] + " ");
             }
-            System.out.println( );
+
+            System.out.println();
         }
 
 
+        for (int i = 0; i < 3; i++) {
+            int temp = 1;
+            for (int j = 0; j < 3; j++) {
+                temp *= ticTacToe[i][j];
+                if (temp == 1) {
+                    System.out.println("Player one won.");
+                    break;
+                } else if (temp == 8) {
+                    System.out.println("Player two won.");
+                    break;
+                } else if (temp == 0) {
+                    System.out.println("Draw");
+                    break;
+                }
+            }
+        }
 
- int [][]ticTacToe = {{1,0,2}, {0,1,2}, {0,2,0}};
-        ticTacToe= new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            int temp = 1;
+            for (int j = 0; j < 3; j++) {
+                temp *= ticTacToe[j][i];
+
+                if (temp == 1) {
+                    System.out.println("Player one won.");
+                    break;
+                } else if (temp == 8) {
+                    System.out.println("Player two won.");
+                    break;
+                } else if (temp == 0) {
+                    System.out.println("No one won");
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            int temp = 1;
+
+            temp *= ticTacToe[i][i];
+
+            if (temp == 1) {
+                System.out.println("Player one won.");
+                break;
+            } else if (temp == 8) {
+                System.out.println("Player two won.");
+                break;
+            } else if (temp == 0) {
+                System.out.println("No one won");
+                break;
+            }
+
+        }
+
+        for (int j = 0; j < 3; j++) {
+            int temp = 1;
+            temp *= ticTacToe[j][j];
+
+            if (temp == 1) {
+                System.out.println("Player one won.");
+                break;
+            } else if (temp == 8) {
+                System.out.println("Player two won.");
+                break;
+            } else if (temp == 0) {
+                System.out.println("No one won");
+                break;
+            }
+        }
+
 
     }
-
 }
+
+
